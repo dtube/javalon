f29c152745fbe81643ac560f3c1c82b20ea41b36
nannal <d.abbott@nannal.com>
2021-03-16 19:56:44 +0200

    Removed alternate getRewardPool

--- index.js
+++ index.js
@@ -395,17 +395,6 @@ var avalon = {
             cb(error)
         })
     },
-    getRewardPool: (cb) => {
-        fetch(avalon.randomNode()+'/rewardpool', {
-            method: 'get',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.json()).then(function(res) {
-            cb(null, res)
-        })
-    },
     getSchedule: (cb) => {
         fetch(avalon.randomNode()+'/schedule', {
             method: 'get',
